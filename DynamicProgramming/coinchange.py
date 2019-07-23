91971c398e3fa6db132bac5bbd12e286bfba74b0
Justin Vu <justin@thinkdataworks.com>
2019-07-23 15:09:04 -0400

    coin change problem

---
+++ DynamicProgramming/coinchange.py
@@ -0,0 +1,13 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1) # creates array length of amount
+        dp[0] = 0 
+        
+        for i in range(amount + 1): 
+            for j in range(len(coins)):
+                if (coins[j] <= i):
+                    dp[i] = min(dp[i], 1 + dp[i - coins[j]])
+        if dp[amount] > amount:
+            return -1
+        else:
+            return dp[amount]
